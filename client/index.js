d43efbf0b98e6c6592b6eda95b358f9010ccdde3
amankkg <amankubanychbek@gmail.com>
2017-10-29 01:59:03 +0600

    add babel plugins, eslint, config is WIP

--- client/index.js
+++ client/index.js
@@ -4,16 +4,16 @@ class Animal {}
 
 class Dog extends Animal {
   woof() {
-    console.log('woof');
+    console.log("woof");
   }
 }
 
 class Cat extends Animal {
   meow() {
-    console.log('meow');
+    console.log("meow");
   }
 }
 
-const dogs : Dog[] = [new Dog()];
-const animals : Animal[] = dogs;
+const dogs: Dog[] = [new Dog()];
+const animals: Animal[] = dogs;
 animals.push(new Cat());
