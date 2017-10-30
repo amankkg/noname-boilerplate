57f9de7a743b6169a97209019b5e5cc594251945
amankkg <amankubanychbek@gmail.com>
2017-10-31 01:24:22 +0600

    prettier-eslint test json commit

--- client/index.js
+++ client/index.js
@@ -1,11 +1,11 @@
 // @flow
-import "babel-polyfill";
-import * as React from "react";
-import { render } from "react-dom";
+import 'babel-polyfill'
+import * as React from 'react'
+import { render } from 'react-dom'
 
-import TextInput from "components/TextInput";
+import TextInput from 'components/TextInput'
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root')
 if (rootElement != null) {
-  render(<TextInput />, rootElement);
+  render(<TextInput />, rootElement)
 }
